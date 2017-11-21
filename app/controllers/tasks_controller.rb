ecf66c8b21fce9d18932c78a333c15d242a5a6b6
G VDH <vanderhaegen.gauthier@gmail.com>
2017-11-21 12:10:05 +0100

    Finished back-end, need to make front

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :destroy, :edit, :update]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:details)
+  end
+end
